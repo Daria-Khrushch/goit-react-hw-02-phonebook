639c1b3a26342c238bacaddada8f32f930d3b964
Daria Khrushch <Daria140694@gmail.com>
2021-12-09 15:08:43 +0200

    nex fixes

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 import s from './ContactList.module.css';
 
 const ContactList = ({ contacts, onDeleteContact }) => (
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import PropTypes from 'prop-types';
 import s from './Filter.module.css';
 
 const Filter = ({ value, onChange }) => (
